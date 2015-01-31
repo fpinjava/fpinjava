5e1ffd300bc679c16f481d11b0ee0daad2dddba8
Pierre-Yves Saumont <pys@volga.fr>
2015-01-31 09:32:34 +0100

    Added imperative equivalent for listing 3.11

--- fpinjava-parent/fpinjava-makingjavafunctional-solutions/src/main/java/com/fpinjava/makingjavafunctional/listing03_11/Store.java
+++ fpinjava-parent/fpinjava-makingjavafunctional-solutions/src/main/java/com/fpinjava/makingjavafunctional/listing03_11/Store.java
@@ -14,7 +14,9 @@ public class Store {
     Product toothPaste = new Product("Tooth paste", 1.5, 0.5);
     Product toothBrush = new Product("Tooth brush", 3.5, 0.3);
 
-    List<OrderLine> order = list(new OrderLine(toothPaste, 2), new OrderLine(toothBrush, 3));
+    List<OrderLine> order = list(
+        new OrderLine(toothPaste, 2), 
+        new OrderLine(toothBrush, 3));
 
     double weight = foldLeft(order, 0.0, x -> y -> x + y.getAmount());
     double price = foldLeft(order, 0.0, x -> y -> x + y.getWeight());
--- fpinjava-parent/fpinjava-makingjavafunctional-solutions/src/main/java/com/fpinjava/makingjavafunctional/listing03_11/StoreImperative.java
+++ fpinjava-parent/fpinjava-makingjavafunctional-solutions/src/main/java/com/fpinjava/makingjavafunctional/listing03_11/StoreImperative.java
@@ -1,5 +1,29 @@
 package com.fpinjava.makingjavafunctional.listing03_11;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fpinjava.makingjavafunctional.listing03_10.OrderLine;
+import com.fpinjava.makingjavafunctional.listing03_10.Product;
+
 public class StoreImperative {
 
+  public static void main(String[] args) {
+
+    Product toothPaste = new Product("Tooth paste", 1.5, 0.5);
+    Product toothBrush = new Product("Tooth brush", 3.5, 0.3);
+
+    List<OrderLine> order = new ArrayList<OrderLine>();
+    order.add(new OrderLine(toothPaste, 2));
+    order.add(new OrderLine(toothBrush, 3));
+
+    double price = 0.0;
+    double weight = 0.0;
+    for (OrderLine orderLine : order) {
+      weight += orderLine.getAmount();
+      price += orderLine.getWeight();
+    }
+    System.out.println(String.format("Total price: %s", price));
+    System.out.println(String.format("Total weight: %s", weight));
+  }
 }
