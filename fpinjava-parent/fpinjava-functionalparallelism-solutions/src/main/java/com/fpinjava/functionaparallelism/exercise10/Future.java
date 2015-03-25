939c8fe4ba40a83b1e82e67fd99526ed52cffb20
Pierre-Yves Saumont <pys@volga.fr>
2015-03-25 22:46:22 +0100

    Removed bad package

--- fpinjava-parent/fpinjava-functionalparallelism-solutions/src/main/java/com/fpinjava/functionaparallelism/exercise10/Future.java
+++
@@ -1,9 +0,0 @@
-package com.fpinjava.functionaparallelism.exercise10;
-
-import java.util.function.Consumer;
-
-
-public abstract class Future<A> {
-  
-  abstract void apply(Consumer<A> k);
-}
--- fpinjava-parent/fpinjava-functionalparallelism-solutions/src/main/java/com/fpinjava/functionaparallelism/listing06/Future.java
+++
@@ -1,9 +0,0 @@
-package com.fpinjava.functionaparallelism.listing06;
-
-import java.util.function.Consumer;
-
-
-public abstract class Future<A> {
-
-  abstract void apply(Consumer<A> k);
-}
