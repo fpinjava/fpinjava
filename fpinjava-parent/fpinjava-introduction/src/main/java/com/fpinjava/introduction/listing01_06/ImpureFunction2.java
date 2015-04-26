4e9da675607851831d77f8fbb05a058ad5697616
athymos <athymos@gmail.com>
2015-04-26 21:08:34 +0200

    Removes unused code

--- fpinjava-parent/fpinjava-introduction/src/main/java/com/fpinjava/introduction/listing01_06/ImpureFunction2.java
+++ fpinjava-parent/fpinjava-introduction/src/main/java/com/fpinjava/introduction/listing01_06/ImpureFunction2.java
@@ -6,16 +6,10 @@ public class ImpureFunction2 {
 
     StringBuilder x = new StringBuilder("Hello, ");
 
-    StringBuilder y = x.append("Functional World!");
-
     String r1 = x.append("Functional World!").toString();
     System.out.println(r1);
 
     String r2 = x.append("Functional World!").toString();
     System.out.println(r2);
-  }
-
-  public static String sayHello(String string) {
-    return "Hello, " + string + "!";
   }
 }
